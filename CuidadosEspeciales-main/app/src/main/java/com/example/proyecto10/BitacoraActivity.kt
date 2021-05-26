01c64de44270d14659c7e577986048f491ec2005
XxPanchoPanteraxX <78341976+XxPanchoPanteraxX@users.noreply.github.com>
2021-05-26 06:07:02 -0700

    Agrega notas

--- CuidadosEspeciales-main/app/src/main/java/com/example/proyecto10/BitacoraActivity.kt
+++ CuidadosEspeciales-main/app/src/main/java/com/example/proyecto10/BitacoraActivity.kt
@@ -6,9 +6,21 @@ import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import com.example.proyecto10.Notas.AdaptadorNotas
+import com.example.proyecto10.Notas.AgregarNotaActivity
+import com.example.proyecto10.Notas.Nota
+import kotlinx.android.synthetic.main.activity_agregar_nota.*
+import kotlinx.android.synthetic.main.activity_bitacora.*
 import kotlinx.android.synthetic.main.activity_profile_doc.*
+import kotlinx.android.synthetic.main.activity_profile_doc.ibtn_abrir
+import kotlinx.android.synthetic.main.activity_profile_doc.ibtn_ambulance
+import kotlinx.android.synthetic.main.activity_profile_doc.ibtn_botiquin
+import kotlinx.android.synthetic.main.activity_profile_doc.ibtn_buscar
+import java.io.*
 
 class BitacoraActivity : AppCompatActivity() {
+
+    var lstNotas=ArrayList<Nota>()
 
     private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)}
     private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)}
@@ -36,6 +48,19 @@ class BitacoraActivity : AppCompatActivity() {
             val intent = Intent(this, ambulanceActivity::class.java)
             startActivity(intent)
         }
+
+        leerNotas()
+
+        var adapter = AdaptadorNotas(lstNotas, this)
+
+        lvNotas.adapter = adapter
+
+
+        nuevaNota.setOnClickListener{
+            val intent = Intent(this,AgregarNotaActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
     private fun onAddButtonClicked() {
@@ -83,4 +108,56 @@ class BitacoraActivity : AppCompatActivity() {
             ibtn_buscar.isClickable = false
         }
     }
+    private fun leerNotas(){
+        lstNotas.clear()
+
+        var carpeta = File(ubicacion().absolutePath)
+
+        if(carpeta.exists()){
+            var archivos=carpeta.listFiles()
+            if(archivos!=null){
+                archivos.forEach {
+                    leerArchivo(it)
+                }
+            }
+        }
+    }
+
+    private fun leerArchivo(it: File?) {
+        val fis= FileInputStream(it)
+        val di= DataInputStream(fis)
+        val br= BufferedReader(InputStreamReader(di))
+
+        var strLine: String? = br.readLine()
+        var contenido=""
+
+        while(strLine!=null){
+            contenido+=strLine
+            strLine=br.readLine()
+        }
+
+        br.close()
+        di.close()
+        fis.close()
+
+        var titulo=it!!.name.substring(0, it!!.name.length-4)
+
+        var nota= Nota(titulo, contenido)
+        lstNotas.add(nota)
+    }
+
+    private fun ubicacion(): File {
+        val carpeta = File(getExternalFilesDir(null), "notas")
+        if(!carpeta.exists()){
+            carpeta.mkdir()
+        }
+        return carpeta
+    }
+
+    private fun notasDePrueba() {
+        lstNotas.add(Nota("Paracetamol","Me toca cada 8 horas."))
+        lstNotas.add(Nota("Diclofenaco","Solo tomar cuando presente inflamación."))
+        lstNotas.add(Nota("Horario","Tomar 2 pastillas a las 11:00 AM y 1 pastilla a la 1:00 PM"))
+    }
+
 }
---
+++ CuidadosEspeciales-main/app/src/main/java/com/example/proyecto10/Notas/Nota.kt
@@ -0,0 +1,3 @@
+package com.example.proyecto10.Notas
+
+data class Nota (var titulo:String, var contenido:String)
